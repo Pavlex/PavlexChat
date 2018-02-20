716742e35c29b116a5af2cbe811504fa973e9d96
lexlip <leha.kudaev@mail.ru>
2018-02-20 23:25:46 +0300

    ввв

--- output.php
+++ output.php
@@ -2,13 +2,17 @@
 	if(isset($_POST["update"])){
 		$db = mysql_connect("localhost","9046936062", "Pavlex","9046936062_users");
 		mysql_select_db ("9046936062_users",$db);
-		$query = mysql_query("SELECT 'users.login', 'message.message_text' FROM  `message` ,  `users` WHERE 'users.id_user' = 'message.id_user' ORDER BY time_message");
+		$query = mysql_query("SELECT users.login, message.message_text FROM  `message` ,  `users` WHERE users.id_user = message.id_user ORDER BY time_message");
 		$row = mysql_fetch_array($query);
 		$result = array();
-		for($i = 0; i<count($row);$i++){
-			$result[$i]["name"] = $row['login'];
-			$result[$i]["message"] = $row['message_text'];
-		}
+		$i = 0;
+		while($row=mysql_fetch_array($query))
+    {
+		$result[$i]["name"] = $row['login'];
+		$result[$i]["message"] = $row['message_text'];
+		$i++;
+	}
+
 		echo json_encode($result);
 	}
 ?>
--- sendMsg.php
+++ sendMsg.php
@@ -1,14 +1,11 @@
 <?php
-  $msg = $_POST['msg'];
-  $login = $_POST['login'];
-  echo "SELECT id_user FROM `users` where login='$login'"."<br>";
-  
+  $msg = $_POST['message'];
+  $login = $_POST['name'];
+
 //удаление лишних пробелов
 //подключение к бд
   include("bd.php");
-  $row1 = mysql_fetch_array(mysql_query("SELECT id_user FROM `users` where login='$login'"));
-  echo "INSERT INTO message (message_text, id_user) VALUES('$msg', ".$row1['id_user'].")"."<br>";
-  $result=mysql_query("INSERT INTO message (message_text, id_user) VALUES('$msg', ".$row1['id_user'].")");
+  $result=mysql_query("INSERT INTO message (message_text, id_user, time_message) VALUES('$msg', ".$login.", NOW())");
+  echo($result);
   mysql_close($db);
-  header('Location: output.php');
 ?>
